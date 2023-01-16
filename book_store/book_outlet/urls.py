9b4284754df2ab8580af8ee3487434d861be8729
delia gt <delissqt@gmail.com>
2023-01-16 16:14:53 -0600

    Rendering the Details Page

--- book_store/book_outlet/urls.py
+++ book_store/book_outlet/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("<int:id>", views.book_detail, name="book_detail")
 ]
--- book_store/book_outlet/views.py
+++ book_store/book_outlet/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from .models import Book
 
@@ -7,3 +8,17 @@ from .models import Book
 def index(request):
     books = Book.objects.all()
     return render(request, "book_outlet/index.html", {"books": books})
+
+
+def book_detail(request, id):
+    
+    #book = Book.objects.get(pk=id)
+
+    book = get_object_or_404(Book, pk=id)
+
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_bestseller": book.is_bestselling
+    })
