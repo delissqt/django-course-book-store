e14631a9070e9d49a13f34e57cd902aa15c16646
delia gt <delissqt@gmail.com>
2023-01-17 16:54:48 -0600

    More Config Options
    Displaying Filter in Admin Panel

--- book_store/book_outlet/admin.py
+++ book_store/book_outlet/admin.py
@@ -6,5 +6,6 @@ from .models import Book
 class BookAdmin(admin.ModelAdmin):
     # readonly_fields = ("slug",)
     prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
 
 admin.site.register(Book, BookAdmin)
