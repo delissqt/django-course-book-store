16a8eb924bac28c28ab8ea447abdd921686b6848
delia gt <delissqt@gmail.com>
2023-01-20 11:02:59 -0600

    Managing Relations in Admin

--- book_store/book_outlet/models.py
+++ book_store/book_outlet/models.py
@@ -10,6 +10,14 @@ class Author(models.Model):
     last_name = models.CharField(max_length=50)
 
 
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+    def __str__(self):
+        return self.full_name()
+
+
 class Book(models.Model):
     title = models.CharField(max_length=40)
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
